23428f6e1346529e89534bef4df747acf7107d12
Roman Tez <tez.romach@gmail.com>
2017-02-28 09:10:38 +0300

    New shape Rectangle and its child Square

---
+++ SimpleGeometry/GeometryLib/Shapes/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryLib.Shapes
+{
+    public class Rectangle : Shape
+    {
+        public double Side1 { get; }
+        public double Side2 { get; }
+
+        public Rectangle(double side1, double side2)
+        {
+            Side1 = GetNormilizedSide(side1);
+            Side2 = GetNormilizedSide(side2);
+        }
+
+        public override double Area() => Side1 * Side2;
+
+        public override string Print() =>
+            $"Rectangle: sides: {Side1} {Side2}, area = {Area()}";
+        private double GetNormilizedSide(double side)
+        {
+            return side > 0 ? side : 0;
+        }
+    }
+}
---
+++ SimpleGeometry/GeometryLib/Shapes/Square.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryLib.Shapes
+{
+    /// <summary>
+    /// Квадрат
+    /// </summary>
+    public class Square : Rectangle
+    {
+        public Square(double side) : base(side, side) { }
+
+        public override string Print() =>
+            $"Square: side: {Side1}, area = {Area()}";
+    }
+}
